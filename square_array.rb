2dbf5f49493dba6d025fa4411a5142b08641b690
Michael Murray <cloud19912003@live.com>
2020-09-16 15:51:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -8,6 +8,6 @@
 #end
 
 
-def square_array(array) new_array = [] array.each { |number| new_array << yield number } new_array end
+def square_array(array) new_array = [] array.each { |number| new_array << yield (number) } new_array end
   
   
