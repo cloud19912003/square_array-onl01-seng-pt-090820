717199bb555ab695183200a1ce523fd92420346f
Michael Murray <cloud19912003@live.com>
2020-09-16 15:09:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  new_square_array = []
-  new_square_array = array.each do |number|
+  square_array = array.each do |number|
     number * 2 
-  return new_square_array
   end 
 end
