b3ec5b8cba201de02fb1752e52e46bdcd8ae46fc
Michael Murray <cloud19912003@live.com>
2020-09-16 15:52:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,8 @@
-#def square_array(array)
-#  new_square_array = []
-#  array.each do |number|
-#    number * 2 
-#    new_square_array << number
-#    new_square_array
-#  end
-#end
-
-
-def square_array(array) new_array = [] array.each { |number| new_array << yield (number) } new_array end
-  
-  
+def square_array(array)
+  new_square_array = []
+  array.each do |number|
+    number * 2 
+    new_square_array << number
+    new_square_array
+  end
+end
