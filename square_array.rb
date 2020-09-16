f7e5073a500b00044cb2595b3e81f06f56bb739e
Michael Murray <cloud19912003@live.com>
2020-09-16 15:44:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
   new_square_array = []
   array.each do |number|
     yield(number)
-      number * 2 
-    end
+    number * 2 
     new_square_array << number
   end 
   new_square_array
