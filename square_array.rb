9065f5ea0374549c1013e6544e6e7ee76a0757dd
Michael Murray <cloud19912003@live.com>
2020-09-16 15:08:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   new_square_array = []
-  new_square_array << array.each do |number|
+  new_square_array = array.each do |number|
     number * 2 
   return new_square_array
   end 
